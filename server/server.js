51f5c817ce28b175799b641f71a56188f2c1e404
Taylor Abbott <68927611+taylornicole906@users.noreply.github.com>
2021-01-22 20:51:55 -0800

    pls work

--- server/server.js
+++ server/server.js
@@ -28,14 +28,12 @@ io.on('connection', client => {
         clientRooms[client.id] = roomName;
         client.emit('gameCode', roomName);
         client.join(roomName);
-        client.emit('init', 1);
-   
         io.in(roomName).emit('displayWords', currUsedWords);  
         io.in(roomName).emit('getRedList', redList);
         io.in(roomName).emit('getBlueList', blueList);
         io.in(roomName).emit('getGrayList', grayList);
         io.in(roomName).emit('getDeathWord', deathWord);
-      
+        client.emit('init');
     }
 
     function handleJoinGame(roomName){
