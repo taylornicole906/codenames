fea022fda32dec973a2ccea22fbe85d66a1b357e
Taylor Abbott <68927611+taylornicole906@users.noreply.github.com>
2021-01-22 21:04:57 -0800

    :/

--- frontend/index.js
+++ frontend/index.js
@@ -50,6 +50,7 @@ function buttonFunction(e) {
 spymasterButton.addEventListener('click', spymaster)
 
 function spymaster(){
+
   for (let i = 0; i < 25; i++){
     var button  = document.getElementById(i+1);
     if (redList.includes(button.textContent)){
@@ -86,7 +87,23 @@ function init() {
   gameScreen.style.display = "block";
   spyMaster = false;
   gameActive = true;
-  spymaster();
+  for (let i = 0; i < 25; i++){
+    var button  = document.getElementById(i+1);
+    if (redList.includes(button.textContent)){
+        button.style.background = "rgba(242, 155, 160, 1)";
+    }
+    else if (blueList.includes(button.textContent)){
+        button.style.background="rgba(149, 212, 212, 1)";
+    }
+    else if (grayList.includes(button.textContent)){
+        button.style.background="white";
+    }
+    else if (deathWord.includes(button.textContent)){
+        button.style.background = "black";
+        button.style.color = "white"         
+    }
+    button.disabled = true;
+ 
 }
 
 function handleInit(msg){
